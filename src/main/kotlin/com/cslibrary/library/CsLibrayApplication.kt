b868973449bddc14c3b9d71a6b3e31b654ed31b1
KangDroid <hyunwoo286@gmail.com>
2021-04-30 11:43:28 +0900

    [Main] Initial Project Setup

---
+++ src/main/kotlin/com/cslibrary/library/CsLibrayApplication.kt
@@ -0,0 +1,11 @@
+package com.cslibrary.library
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class CsLibrayApplication
+
+fun main(args: Array<String>) {
+	runApplication<CsLibrayApplication>(*args)
+}
---
+++ src/test/kotlin/com/cslibrary/library/CsLibrayApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.cslibrary.library
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class CsLibrayApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
