cd6aa76bfce91ed605231b305fbbb0217c00f629
KangDroid <hyunwoo286@gmail.com>
2021-04-30 12:12:55 +0900

    [Entity] Add Initial User Entity

---
+++ src/main/kotlin/com/cslibrary/library/data/User.kt
@@ -0,0 +1,40 @@
+package com.cslibrary.library.data
+
+import org.bson.types.ObjectId
+import org.springframework.data.annotation.Id
+import org.springframework.data.mongodb.core.mapping.Document
+import org.springframework.security.core.GrantedAuthority
+import org.springframework.security.core.authority.SimpleGrantedAuthority
+import org.springframework.security.core.userdetails.UserDetails
+import java.util.stream.Collectors
+
+@Document(collection="user")
+data class User(
+    @Id
+    var id: ObjectId,
+    var userId: String,
+    var userPassword: String,
+    var userName: String,
+    var userPhoneNumber: String,
+    var roles: Set<String>,
+    var leftTime: Long,
+    var reservedSeatNumber: String,
+    var userState: String
+): UserDetails {
+    override fun getAuthorities(): Collection<GrantedAuthority?>? {
+        return roles.stream()
+            .map { role: String? ->
+                SimpleGrantedAuthority(
+                    role
+                )
+            }
+            .collect(Collectors.toList())
+    }
+
+    override fun getPassword() = userPassword
+    override fun getUsername(): String = userName
+    override fun isAccountNonExpired(): Boolean = true
+    override fun isAccountNonLocked(): Boolean = true
+    override fun isCredentialsNonExpired(): Boolean = true
+    override fun isEnabled(): Boolean = true
+}
