d60e8a7934bcca23206e47122b1cdc49bf87c14d
KangDroid <hyunwoo286@gmail.com>
2021-05-06 23:58:22 +0900

    [UserController] Initiate Login Function

--- src/main/kotlin/com/cslibrary/library/controller/UserController.kt
+++ src/main/kotlin/com/cslibrary/library/controller/UserController.kt
@@ -1,11 +1,14 @@
 package com.cslibrary.library.controller
 
+import com.cslibrary.library.data.dto.request.LoginRequest
 import com.cslibrary.library.data.dto.request.RegisterRequest
+import com.cslibrary.library.data.dto.response.LoginResponse
 import com.cslibrary.library.data.dto.response.RegisterResponse
 import com.cslibrary.library.service.UserService
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
@@ -18,5 +21,13 @@ class UserController(
         return ResponseEntity
             .status(HttpStatus.OK)
             .body(registerResponse)
+    }
+
+    @PostMapping("/api/v1/login")
+    fun loginUser(@RequestBody loginRequest: LoginRequest): ResponseEntity<LoginResponse> {
+        val loginResponse: LoginResponse = userService.loginUser(loginRequest)
+        return ResponseEntity
+            .status(HttpStatus.OK)
+            .body(loginResponse)
     }
 }
--- src/test/kotlin/com/cslibrary/library/controller/UserControllerTest.kt
+++ src/test/kotlin/com/cslibrary/library/controller/UserControllerTest.kt
@@ -1,8 +1,14 @@
 package com.cslibrary.library.controller
 
+import com.cslibrary.library.data.User
+import com.cslibrary.library.data.UserRepository
+import com.cslibrary.library.data.dto.request.LoginRequest
 import com.cslibrary.library.data.dto.request.RegisterRequest
+import com.cslibrary.library.data.dto.response.LoginResponse
 import com.cslibrary.library.data.dto.response.RegisterResponse
 import org.assertj.core.api.Assertions.assertThat
+import org.junit.After
+import org.junit.Before
 import org.junit.Test
 import org.junit.runner.RunWith
 import org.springframework.beans.factory.annotation.Autowired
@@ -10,6 +16,8 @@ import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.boot.test.web.client.TestRestTemplate
 import org.springframework.boot.test.web.client.postForEntity
 import org.springframework.boot.web.server.LocalServerPort
+import org.springframework.data.mongodb.core.MongoTemplate
+import org.springframework.data.mongodb.core.query.Query
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.test.context.junit4.SpringRunner
@@ -22,6 +30,18 @@ class UserControllerTest {
 
     @Autowired
     private lateinit var restTemplate: TestRestTemplate
+
+    @Autowired
+    private lateinit var userRepository: UserRepository
+
+    @Autowired
+    private lateinit var mongoTemplate: MongoTemplate
+
+    @Before
+    @After
+    fun destroy() {
+        mongoTemplate.remove(Query(), User::class.java)
+    }
 
     @Test
     fun is_registerUserWorksWell() {
@@ -39,5 +59,28 @@ class UserControllerTest {
         assertThat(registerResponse.hasBody()).isEqualTo(true)
         assertThat(registerResponse.body).isNotEqualTo(null)
         assertThat(registerResponse.body!!.registeredId).isNotEqualTo(mockUserRegisterRequest.userId)
+    }
+
+    @Test
+    fun is_loginUserWorksWell() {
+        val serverBaseUrl: String = "http://localhost:${port}"
+        val mockUser: User = User(
+            userId = "kangdroid",
+            userPassword = "test",
+            userName = "testingname",
+            userPhoneNumber = "whatever"
+        )
+        val mockLoginRequest: LoginRequest = LoginRequest(
+            userId = mockUser.userId,
+            userPassword = mockUser.userPassword
+        )
+        userRepository.addUser(mockUser)
+
+        val loginResponse: ResponseEntity<LoginResponse> =
+            restTemplate.postForEntity("${serverBaseUrl}/api/v1/login", mockLoginRequest)
+
+        assertThat(loginResponse.statusCode).isEqualTo(HttpStatus.OK)
+        assertThat(loginResponse.body).isNotEqualTo(null)
+        assertThat(loginResponse.body!!.userToken).isNotEqualTo("")
     }
 }
