7402d2662422a072dbadbf949c78ffd278accfba
KangDroid <hyunwoo286@gmail.com>
2021-05-28 11:04:31 +0900

    [Security] Throw 500 when account is locked

--- src/main/kotlin/com/cslibrary/library/security/CustomUserDetailService.kt
+++ src/main/kotlin/com/cslibrary/library/security/CustomUserDetailService.kt
@@ -1,6 +1,8 @@
 package com.cslibrary.library.security
 
+import com.cslibrary.library.data.User
 import com.cslibrary.library.data.UserRepository
+import com.cslibrary.library.error.exception.UnknownErrorException
 import org.springframework.security.core.userdetails.UserDetails
 import org.springframework.security.core.userdetails.UserDetailsService
 import org.springframework.stereotype.Service
@@ -8,6 +10,10 @@ import org.springframework.stereotype.Service
 @Service
 class CustomUserDetailService(private val userRepository: UserRepository) : UserDetailsService {
     override fun loadUserByUsername(userId: String): UserDetails {
-        return userRepository.findByUserId(userId)
+        val user: User = userRepository.findByUserId(userId)
+        if (!user.isAccountNonLocked) {
+            throw UnknownErrorException("Account seems like locked! Contact Admin for details.")
+        }
+        return user
     }
 }
