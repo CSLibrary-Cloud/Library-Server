d1020d4c0c51e55329743182cddbe10b7ac00569
KangDroid <hyunwoo286@gmail.com>
2021-05-31 18:56:31 +0900

    Admin: Encrypt password

--- src/main/kotlin/com/cslibrary/library/config/AdminConfig.kt
+++ src/main/kotlin/com/cslibrary/library/config/AdminConfig.kt
@@ -2,6 +2,7 @@ package com.cslibrary.library.config
 
 import com.cslibrary.library.data.User
 import com.cslibrary.library.data.UserRepository
+import com.cslibrary.library.service.PasswordEncryptorService
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.context.properties.ConfigurationProperties
 import org.springframework.boot.context.properties.ConstructorBinding
@@ -17,12 +18,15 @@ class AdminConfig(
     @Autowired
     private lateinit var userRepository: UserRepository
 
+    @Autowired
+    private lateinit var passwordEncryptorService: PasswordEncryptorService
+
     @PostConstruct
     fun registerAdminAccount() {
         userRepository.addUser(
             User(
                 userId = "admin",
-                userPassword = masterPassword,
+                userPassword = passwordEncryptorService.encodePlainText(masterPassword),
                 userName = "admin",
                 userPhoneNumber = "",
                 roles = setOf("ROLE_ADMIN")
